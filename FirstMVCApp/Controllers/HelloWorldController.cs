75e0446dbbfa17c4027b4cdfb188faf409a0542e
Rolando Penagos <rpmacal2013@outlook.com>
2022-10-17 11:21:29 -0500

    Section 3. ASP.NET Core 6.0 MVC Blazor Razor EF Core

---
+++ FirstMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,39 @@
+﻿using FirstMVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
+
+        public IActionResult Index()
+        {
+            return View(dogs);
+        }
+
+        public IActionResult Hello()
+        {
+            DogViewModel doggy = new DogViewModel()
+            {
+                Name = "Sultán",
+                Age = 6
+            };
+
+            return View(doggy);
+        }
+
+        public IActionResult Create()
+        {
+            var dogVM = new DogViewModel();
+            return View(dogVM);
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {   
+            dogs.Add(dogViewModel);
+            //return View("Index");
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
